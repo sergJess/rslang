bf61e65e28e37a206090bdc39f977cb55753d3cf
sergJess <serg.fantasy@yandex.ru>
2022-02-21 17:12:06 +0300

    feat: add all sounds

--- rslang/src/ts/app/dictonary/dictonary-item.ts
+++ rslang/src/ts/app/dictonary/dictonary-item.ts
@@ -22,7 +22,7 @@ word.textContent = `${data.word} - ${data.transcription} - ${data.wordTranslate}
 //sound
 const sound = document.createElement('p');
 sound.classList.add('dictonary-sound');
-sound.textContent = 'click to listen Word';
+sound.textContent = 'click to listen the Word';
 sound.onclick = (): void =>{
 	if(player){
 		player.play(`${imgUrl}${data.audio}`);
@@ -36,14 +36,32 @@ img.src = `${imgUrl}${data.image}`;
 // text meaning
 const meaningText = document.createElement('p');
 meaningText.classList.add('dictonary-text');
+const soundMeaning = document.createElement('span');
+soundMeaning.textContent = '(click here to listen)';
+soundMeaning.classList.add('dictonary-sound-2');
+soundMeaning.onclick = ():	void => {
+	if(player){
+		player.play(`${imgUrl}${data.audioMeaning}`);
+	}
+};
 meaningText.innerHTML = `${data.textMeaning}`;
+meaningText.append(soundMeaning);
 const meaningTranslate = document.createElement('p');
 meaningTranslate.classList.add('dictonary-text_translate');
 meaningTranslate.textContent = `${data.textMeaningTranslate}`
 // text example
 const exampleText = document.createElement('p');
 exampleText.classList.add('dictonary-text');
+const soundExample = document.createElement('span');
+soundExample.textContent = '(click here to listen)';
+soundExample.classList.add('dictonary-sound-2');
+soundExample.onclick = (): void => {
+	if(player){
+		player.play(`${imgUrl}${data.audioExample}`);
+	}
+};
 exampleText.innerHTML = `${data.textExample}`;
+exampleText.append(soundExample);
 const exampleTranslate = document.createElement('p');
 exampleTranslate.classList.add('dictonary-text_translate');
 exampleTranslate.textContent = `${data.textExampleTranslate}`;
