080356ff362ae825c8a59c7d29f294b197db3346
sergJess <serg.fantasy@yandex.ru>
2022-02-23 13:22:45 +0300

    feat: add selection of nav sprint game

--- rslang/src/ts/app/app.ts
+++ rslang/src/ts/app/app.ts
@@ -3,24 +3,26 @@ import './base.scss';
 import {getWords} from '../app/api/api';
 import {WORDS_URL, DATA_RESOURCE } from '../app/constants/constants';
 import StartPage from '../app/start-page/start-page';
-import Dictonary from '../app/dictonary/dictonary';
 import SprintPage from '../app/sprint/sprint-page';
+import Dictonary from '../app/dictonary/dictonary';
+
 export default class App {
-	private parent:HTMLElement;
+	private parent: HTMLElement;
 	constructor(parentNode:HTMLElement){
 this.parent =	parentNode;
 	}
-	start():void{
+	start(): void{
 		const startingPage = new StartPage(this.parent);
 //dictonary page starting
 startingPage.dictonaryOnClick = () => {
 	const dictonary =	new Dictonary(this.parent, DATA_RESOURCE);
 	startingPage.clear();
+	// dictonary parts click
 	dictonary.partsDictonary.click = (): void => {
 		dictonary.clearDictonaryItems();
 		dictonary.renderWords(getWords(WORDS_URL, `${dictonary.controllerDictonaryNav.getCounter()}`, `${dictonary.partsDictonary.getControllerValue()}`), DATA_RESOURCE);
 	};
-	//events
+	//events click to choose page of words
 	dictonary.navDictonary.buttonLeftOnClick = (): void =>{
 		dictonary.controllerDictonaryNav.decrementCounter();
 		dictonary.clearDictonaryItems();
@@ -34,21 +36,56 @@ startingPage.dictonaryOnClick = () => {
 	
 		dictonary.navDictonary.setNumber(`${dictonary.controllerDictonaryNav.getCounter() + 1}`);
 	}
-
+// to home
 	dictonary.clickToHome = (): void => {
 		dictonary.clear();
 		startingPage.render();
 	}
-	
+	// render words
 	dictonary.render(getWords(WORDS_URL, '0', '0'));
 
 	};
-// sprint game starting
-	startingPage.toGameSprint = ():void => {
-		startingPage.clear();
-		const pageSprint = new SprintPage(this.parent);
-		console.log('Sprint')};
+// sprint game page starting
+startingPage.toGameSprint = ()=>{
+	startingPage.clear();
+	const sprintGame = new SprintPage(this.parent);
+	sprintGame.clickToHome =	(): void => {
+		sprintGame.clear();
+		startingPage.render();
+	};
+	sprintGame.clickToDictonary = (): void => {
+		const dictonary =	new Dictonary(this.parent, DATA_RESOURCE);
+		sprintGame.clear();
+		// dictonary parts click
+		dictonary.partsDictonary.click = (): void => {
+			dictonary.clearDictonaryItems();
+			dictonary.renderWords(getWords(WORDS_URL, `${dictonary.controllerDictonaryNav.getCounter()}`, `${dictonary.partsDictonary.getControllerValue()}`), DATA_RESOURCE);
+		};
+		//events click to choose page of words
+		dictonary.navDictonary.buttonLeftOnClick = (): void =>{
+			dictonary.controllerDictonaryNav.decrementCounter();
+			dictonary.clearDictonaryItems();
+			dictonary.renderWords(getWords(WORDS_URL, `${dictonary.controllerDictonaryNav.getCounter()}`, `${dictonary.partsDictonary.getControllerValue()}`), DATA_RESOURCE);
+			dictonary.navDictonary.setNumber(`${dictonary.controllerDictonaryNav.getCounter() + 1}`);
+		}
+		dictonary.navDictonary.buttonRightOnClick = (): void =>{
+			dictonary.controllerDictonaryNav.incrementCounter();
+			dictonary.clearDictonaryItems();
+			dictonary.renderWords(getWords(WORDS_URL, `${dictonary.controllerDictonaryNav.getCounter()}`, `${dictonary.partsDictonary.getControllerValue()}`), DATA_RESOURCE);
+		
+			dictonary.navDictonary.setNumber(`${dictonary.controllerDictonaryNav.getCounter() + 1}`);
+		}
+	// to home
+		dictonary.clickToHome = (): void => {
+			dictonary.clear();
+			startingPage.render();
+		}
+		// render words
+		dictonary.render(getWords(WORDS_URL, '0', '0'));
+	};
+	sprintGame.renderStartScreen();
 
+};
 	// start
 startingPage.render();
 
--- rslang/src/ts/app/sprint/sprint-page.ts
+++ rslang/src/ts/app/sprint/sprint-page.ts
@@ -1,15 +1,23 @@
 /* eslint-disable */
 import './sprint-page.scss';
+import Navigation from '../navigation/nav';
 export default class SprintPage{
 	private parent: HTMLElement;
 	private pageSprint = document.createElement('div');
+	private gameFieldInner = document.createElement('div');
+	clickToHome = ():	void => {};
+ clickToDictonary = (): void => {};
 	constructor(parentNode: HTMLElement){
 this.parent =	parentNode;
 this.pageSprint.classList.add('sprint-page');
 this.parent.append(this.pageSprint);
 	}
-	render(): void{
-
+	renderStartScreen(): void{
+		const nav = new Navigation(this.pageSprint); 
+			nav.createNavElement('Домой', this.clickToHome);
+			nav.createNavElement('Словарь', this.clickToDictonary);
+			nav.render();
+			this.pageSprint.append(this.gameFieldInner);
 	}
 	clear(): void{
 		this.pageSprint.remove();
