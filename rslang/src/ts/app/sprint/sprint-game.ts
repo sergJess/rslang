875dabc44978cd6b28cd251edd0829d909d9a2c5
sergJess <serg.fantasy@yandex.ru>
2022-03-03 22:04:06 +0300

    feat: add sprint start page

--- rslang/src/ts/app/sprint/sprint-game.ts
+++ rslang/src/ts/app/sprint/sprint-game.ts
@@ -13,4 +13,20 @@ render(): void{
 	this.gameField.classList.add('game-field');
 	this.gameQuestion.classList.add('game-question');
 }
+renderStart(): void{
+	const startScreen = document.createElement('div');
+	startScreen.classList.add('sprint-start-screen');
+	const buttonStart = document.createElement('button');
+	buttonStart.classList.add('sprint-button-start');
+	buttonStart.textContent = 'Начать игру';
+	buttonStart.onclick = ()=>{
+		this.clearField();
+		this.render();
+	};
+	startScreen.append(buttonStart);
+	this.gameField.append(startScreen);
+}
+clearField(): void{
+	this.gameField.innerHTML = '';
+}
 }
